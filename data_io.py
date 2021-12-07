4b24e663ffa018dc344cc8122c7a2cb488d259f5
Stefaan Haspeslagh <stefaan.haspeslagh@gmail.com>
2021-12-07 17:15:03 +0100

    convert to module

--- data_io.py
+++ data_io.py
@@ -4,7 +4,7 @@ import json
 import pandas as pd
 import os
 from datetime import datetime, time, timedelta
-from synchrotool import Planning, Order, AdhocLegs
+from .synchrotool import Planning, Order, AdhocLegs
 
 
 class Object:
